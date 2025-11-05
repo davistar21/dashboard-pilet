6bb253a4c9e28d34c25a5a8b2323c097616ed194
David <eyitayobembe@gmail.com>
2025-11-05 08:16:50 +0100

    d

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,9 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Explicitly target ESNext to help resolve ESM compatibility issues
+      target: "esnext",
+      loader: "jsx",
+    },
+  },
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    // You can add this to force the output target to ESNext if needed
+    target: "esnext",
   },
   server: {
     watch: {
